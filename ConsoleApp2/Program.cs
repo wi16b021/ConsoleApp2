ec985aa9fe4c4530c43fa70be138c9015cffd8e4
Christoph <Christoph@BURGER>
2017-09-29 11:41:39 +0200

    Try it out pls

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,11 +10,13 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            //test
-            Console.WriteLine();
-            
-            //schreiben
-            //Christoph was here
+
+            NiceTest();
+        }
+
+        static public void NiceTest()
+        {
+            Console.Beep(1200, 3000);
         }
     }
 }
